bc47adb3ed400b47674508ab585beee6f00e390e
Tlyl1111 <yenlinhtranle1111@gmail.com>
2024-03-09 04:25:27 +0700

    Initial commit

---
+++ purchase_logistic/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Purchase Logistic',
+    'version': '1.1',
+    'summary': 'Logistic',
+    'author': 'Linh',
+    'description': ''' ,
+    'license': 'LGPL-3',
+
+    ''',
+    'website': 'https://www.youtube.com/watch?v=KaROApHbMjI&list=PLan8s2_lZPLqccelhcSJuifsoVtY7H3wf&index=13',
+    'depend': [],
+    'data': [
+
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
