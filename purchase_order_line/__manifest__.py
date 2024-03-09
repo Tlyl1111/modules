ae4a7f15c5da0c54ff45fa2c0b1635669b0365d0
Tlyl1111 <yenlinhtranle1111@gmail.com>
2024-03-09 14:43:23 +0700

    add module purchase order, purchase order line

---
+++ purchase_order_line/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Purchase Order Line',
+    'version': '1.1',
+    'summary': 'Purchase',
+    'author': 'Linh',
+    'description': ''' ,
+    'license': 'LGPL-3',
+
+    ''',
+    'website': '',
+    'depend': [],
+    'data': [
+
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
